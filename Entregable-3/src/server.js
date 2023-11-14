3c624034794937b77893238eb340c67d253fa6dc
ferminarpone <arponefermin@gmail.com>
2023-11-14 17:56:12 +0100

    Testing

--- Entregable-3/src/server.js
+++ Entregable-3/src/server.js
@@ -2,16 +2,38 @@ import express from "express";
 import { ProductManager } from "./ProductManager.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 
 const manager = new ProductManager("./src/data/Products.json");
-
-app.get("/products", async (req, res) => {
-  const products = manager.getProducts();
-  res.json(products)
+const products = manager.getProducts();
+app.get("/products", (req, res) => {
+  const { limit } = req.query;
+  if (limit) {
+    if (limit > products.length) {
+      res.send({
+        mensaje: `Solo existen ${products.length} productos`,
+      });
+    } else {
+      const limitProducts = [];
+      products.map((el, index) => index < limit && limitProducts.push(el));
+      res.json(limitProducts);
+    }
+  } else {
+    res.json(products);
+  }
 });
 
-app.listen(PORT, () => console.log("Server listening in port 5000"));
- 
+app.get("/products/:pid", (req, res) => {
+  const { pid } = req.params;
+  const fetch = products.find((el) => el.id === Number(pid));
+  if (fetch) {
+    const productId = manager.getProductById(Number(pid));
+    return res.json(productId);
+  }
+  res.json({
+    error: `El producto con id: ${pid} no existe `,
+  });
+});
 
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
