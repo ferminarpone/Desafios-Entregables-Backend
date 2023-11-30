59bcf659e04b99e95d124c7422345e6be600bd9a
ferminarpone <arponefermin@gmail.com>
2023-11-30 15:04:06 +0100

    Home

--- Entregable-4/src/routes/views.routes.js
+++ Entregable-4/src/routes/views.routes.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
+import { ProductManager } from "../classes/products/ProductManager.js";
+import { __dirname } from "../utils.js";
 
 const router = Router();
 
-/* router.get("/", (req, res) => {
-  const productos = { user: "Fermin" };
-  res.render("home", {
-    user: productos.user,
-  });
-}); */
+const manager = new ProductManager(`${__dirname}/data/Products.json`);
 router.get("/", (req, res) => {
+  const products = manager.getProducts();
+  res.render("home", { products });
+  /*   try {
+  } catch (e) {
     res.render("home", {
-      title: "Titulo nuevo nuevo",
-      name: "Eric",
-      fileCss: "styles.css",
+      error: e.message,
     });
-  });
-  
+  } */
+});
+
 export default router;
--- Entregable-4/src/server.js
+++ Entregable-4/src/server.js
@@ -1,27 +1,33 @@
 import express from "express";
-import { __dirname } from "./utils.js";
 import handlebars from "express-handlebars";
 import viewRouter from "./routes/views.routes.js";
+import { __dirname } from "./utils.js";
 
 const app = express();
 const PORT = 8080;
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configuramos el engine
 app.engine(
   "hbs",
   handlebars.engine({
-    extname: ".hbs",
+    // index.hbs
+    extname: "hbs",
+    // Plantilla principal
     defaultLayout: "main",
   })
 );
+
+// Seteamos nuestro motor
 app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
-app.use(express.static(__dirname + "/public"));
-
-
+app.set("views", `${__dirname}/views`);
+// Public
+app.use(express.static(`${__dirname}/public`));
+// Routes
 app.use("/", viewRouter);
-
-
-
 
 /* app.use(express.json());
 app.use(express.urlencoded({ extended: true })); */
