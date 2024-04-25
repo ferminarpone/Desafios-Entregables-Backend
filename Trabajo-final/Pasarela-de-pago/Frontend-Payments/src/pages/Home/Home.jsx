86e8a03590c1dc658d44a0bba8168e958c41042d
ferminarpone <arponefermin@gmail.com>
2024-04-25 16:45:15 -0300

    Payments

--- Trabajo-final/Pasarela-de-pago/Frontend-Payments/src/pages/Home/Home.jsx
+++ Trabajo-final/Pasarela-de-pago/Frontend-Payments/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import styles from './Home.module.scss';
 const Home = () => {
     return (<>
         <div className={styles.container}>
-            <h1 className={styles.title}>APRENDIENDO PASARELAS DE PAGOo</h1>
+            <h1 className={styles.title}>APRENDIENDO PASARELAS DE PAGO</h1>
         </div>
         <div className={styles.container}>
             <p className={styles.title}>Selecciona la pasarela de Stripe para continuar</p>
